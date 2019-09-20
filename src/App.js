ea3ad344e75a081917686751eb68ed339850f508
Gatrrr <davidgatrmacaulay@gmail.com>
2019-09-20 11:00:38 -0700

    MVP reached

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
 
 
-export default function App() {
+function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <Route exact path="/characters" component={CharacterList} />
+      <Route path="/" components={WelcomePage} />
     </main>
   );
 }
+
+export default App; 
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import SearchForm from './SearchForm';
+import styled from 'styled-components'
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+const CharacterList = props =>  {
+  const [characters, setCharacters] = useState([])
+    useEffect(() => {
+      const getCharacter = () => {
+        axios 
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.log("Error.....", error)
+        })
+      }
+    getCharacter();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm characters={characters}/>
+      {characters.map(character => (
+        <CharacterDetails key={character.id} character={character} />
+      ))}
     </section>
   );
 }
+
+function CharacterDetails({ character }) {
+  const { name, species } = character; 
+  return (
+    <StyledContainer>
+      <StyledDiv>
+        <h2>{name}</h2>
+        <h3>{species}</h3>
+      </StyledDiv>
+    </StyledContainer>
+  );
+}
+
+export default CharacterList;
+
+const StyledDiv = styled.div`
+text-align: center;
+border: 2px dashed black;
+margin-top: 8px;
+margin-bottom: 8px;
+`
+
+const StyledContainer = styled.div`
+background-color: limegreen;
+`
+
+// TODO: Add API Request here - must run in `useEffect`
+//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
